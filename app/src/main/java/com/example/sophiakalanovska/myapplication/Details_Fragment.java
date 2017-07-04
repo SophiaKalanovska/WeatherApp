e8bc2fcfd336942750881323f5093be368a2246f
sophiakalanovska <sophia.kalanovska@kcl.ac.uk>
2017-07-04 18:10:24 +0300

    fragment added

---
+++ app/src/main/java/com/example/sophiakalanovska/myapplication/Details_Fragment.java
@@ -0,0 +1,89 @@
+package com.example.sophiakalanovska.myapplication;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.sophiakalanovska.myapplication.data.Day;
+import com.example.sophiakalanovska.myapplication.data.Icon;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Details_Fragment extends Fragment {
+
+
+    public static Details_Fragment newInstance(Day day) {
+        Details_Fragment f = new Details_Fragment();
+        Bundle args = new Bundle();
+        args.putSerializable("day", day);
+        f.setArguments(args);
+        return f;
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
+
+        View secoundView = inflater.inflate(R.layout.detail, container, false);
+
+        // my_child_toolbar is defined in the layout file
+        Toolbar myChildToolbar =
+                (Toolbar) secoundView.findViewById(R.id.toolbarChild);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(myChildToolbar);
+
+        // Get a support ActionBar corresponding to this toolbar
+        ActionBar ab = ((AppCompatActivity) getActivity()).getSupportActionBar();
+
+        // Enable the Up button
+        ab.setDisplayHomeAsUpEnabled(true);
+        ab.setLogo(R.drawable.ic_sun_logo);
+
+
+        TextView txtToday = (TextView) secoundView.findViewById(R.id.textdate);
+        TextView txtTitleDetail = (TextView) secoundView.findViewById(R.id.textTitleDetail);
+        TextView txtmaxTempratureToday = (TextView) secoundView.findViewById(R.id.textmaxdetail);
+        TextView txtminTempratureToday = (TextView) secoundView.findViewById(R.id.textmindetail);
+        TextView textDesc = (TextView) secoundView.findViewById(R.id.textDesc);
+        ImageView image = (ImageView) secoundView.findViewById(R.id.imageViewDesc);
+        TextView hum = (TextView) secoundView.findViewById(R.id.humidity);
+        TextView press = (TextView) secoundView.findViewById(R.id.pressure);
+        TextView wind = (TextView) secoundView.findViewById(R.id.wind);
+
+
+        Day day = (Day) getArguments().get("day");
+
+        txtToday.setText("" + day.getDate());
+        txtTitleDetail.setText(day.getTitle());
+        txtmaxTempratureToday.setText(getString(R.string.maxtemp, "" + day.getMaxtemp()));
+        txtminTempratureToday.setText(getString(R.string.mintemp, "" + day.getMintemp()));
+        image.setImageResource(Icon.bigIcon(day.getImage()));
+        textDesc.setText(day.getDescription());
+        hum.setText(getString(R.string.humidity, day.getHumidity()));
+        press.setText(getString(R.string.pressure, "" + day.getPressure()));
+        wind.setText(getString(R.string.wind, "" + day.getWind()));
+
+        return secoundView;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Log.d("sofi", "onOptionsItemSelected() called with: item = [" + item + "]");
+       // item.getItemId() == android.R.id.home
+        return super.onOptionsItemSelected(item);
+    }
+}
+
---
+++ app/src/main/java/com/example/sophiakalanovska/myapplication/WeeklyForecastFragment.java
@@ -0,0 +1,138 @@
+package com.example.sophiakalanovska.myapplication;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import com.example.sophiakalanovska.myapplication.Activities.MainActivity;
+import com.example.sophiakalanovska.myapplication.data.DataRepository;
+import com.example.sophiakalanovska.myapplication.data.Day;
+import com.example.sophiakalanovska.myapplication.data.Icon;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.util.List;
+
+
+public class WeeklyForecastFragment extends Fragment implements RecyclerAdapter.DayClickListener {
+
+    private Toolbar toolbar;
+    private RecyclerView recyclerview;
+    private RecyclerAdapter recyclerAdapter;
+    private Settings settings;
+    private DataRepository data;
+    private DataRepository.CallBack callBack;
+    private TextView txtToday;
+    private TextView txtmaxTempratureToday;
+    private TextView txtminTempratureToday;
+    private TextView textDescription;
+    private ImageView image;
+    private List<Day> days;
+
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+
+    // Restore preferences
+
+
+    public static WeeklyForecastFragment newInstance() {
+
+
+        WeeklyForecastFragment fragment = new WeeklyForecastFragment();
+
+        return fragment;
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.weekly_forcast, container, false);
+        settings = new Settings();
+        data = new DataRepository(settings);
+        toolbar = (Toolbar) rootView.findViewById(R.id.weather_bar);
+        toolbar.setTitle("");
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+
+
+        final Switch toggle = (Switch) rootView.findViewById(R.id.switch_b);
+        toggle.setChecked(settings.isFahrenheit());
+        toggle.setOnCheckedChangeListener
+                (new CompoundButton.OnCheckedChangeListener() {
+                     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                         settings.setFahrenheit(isChecked);
+                         data.getDays(callBack);
+                     }
+                 }
+
+                );
+
+
+        recyclerview = (RecyclerView) rootView.findViewById(R.id.sixdays);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(WeatherApplication.getInstance());
+        recyclerview.setLayoutManager(layoutManager);
+        txtToday = (TextView) rootView.findViewById(R.id.textToday);
+        txtmaxTempratureToday = (TextView) rootView.findViewById(R.id.maxtemperatureToday);
+        txtminTempratureToday = (TextView) rootView.findViewById(R.id.mintemperatureToday);
+        textDescription = (TextView) rootView.findViewById(R.id.textDescription);
+        image = (ImageView) rootView.findViewById(R.id.imageView);
+
+        callBack = new DataRepository.CallBack() {
+            @Override
+            public void recieveDays(List<Day> days) {
+                WeeklyForecastFragment.this.days = days;
+                textDescription.setText(days.get(0).getDescription());
+                image.setImageResource(Icon.bigIcon(days.get(0).getImage()));
+                txtToday.setText(getString(R.string.today, days.get(0).getDate()));
+                txtmaxTempratureToday.setText(getString(R.string.maxtemp, days.get(0).getMaxtemp()));
+                txtminTempratureToday.setText(getString(R.string.maxtemp, days.get(0).getMintemp()));
+                recyclerAdapter = new RecyclerAdapter(days.subList(1, days.size()), getContext(), WeeklyForecastFragment.this);
+                recyclerview.setAdapter(recyclerAdapter);
+
+            }
+        };
+        data.getDays(callBack);
+
+
+        View btnNextScreen = rootView.findViewById(R.id.weather_today);
+        btnNextScreen.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View arg0) {
+                onDayClicked(days.get(0));
+
+            }
+        });
+
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(getContext()).addApi(AppIndex.API).build();
+        // Inflate the layout for this fragment
+
+
+        return rootView;
+    }
+
+    @Override
+    public void onDayClicked(Day day) {
+        ((MainActivity) getActivity()).openDetails(day);
+
+    }
+}
